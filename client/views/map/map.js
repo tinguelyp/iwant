0acc513a6632ace5bbc6adf91bf48ef31527ff77
Herve <herve.bonvin@gmail.com>
2015-06-03 15:57:25 +0200

    remove customer icon which was correctly centered

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -15,13 +15,11 @@ Template.map.events({
 
 Template.map.rendered = function() {
 
-  var iWantIcon = L.icon({
-      iconUrl: 'images/favicons/favicon-32x32.png',
-      iconRetinaUrl: 'images/favicons/favicon-32x32.png',
-      iconSize: [32, 32],
-      iconAnchor: [22, 94],
-      popupAnchor: [-3, -76]
-  });
+  // var iWantIcon = L.icon({
+  //     iconUrl: 'images/favicons/favicon-32x32.png',
+  //     iconRetinaUrl: 'images/favicons/favicon-32x32.png'
+      
+  // });
 
   L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
 
@@ -47,7 +45,8 @@ Template.map.rendered = function() {
     if (init_pos) {
       latlng.lat = event.latitude;
       latlng.lng = event.longitude;
-      L.marker([event.latitude, event.longitude], {icon: iWantIcon}).addTo(map);
+      // L.marker([event.latitude, event.longitude], {icon: iWantIcon}).addTo(map);
+      L.marker([event.latitude, event.longitude]).addTo(map);
       circle = L.circle([event.latitude, event.longitude], circle_size).addTo(map);
       console.log(circle);
       init_pos = false;
